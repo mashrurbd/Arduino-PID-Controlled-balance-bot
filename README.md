7db66e6b53a4544365fb2f05656e198258f4a16b
mashrurbd <mohsinmashrur@gmail.com>
2021-02-11 13:33:21 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Arduino code for a 2 wheel balanced bot.
---
+++ hello.py
@@ -0,0 +1,21 @@
+# Practise file
+# Please do not treat it otherwise!
+
+import time;
+
+
+def demo(x,y):
+    print('')
+    print(f'🔥⚡ Sum calculated from function: {x+y}');
+    print(f'Printing time { time.time() }')
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    # print('Calling main function...');
+    
+    demo(1,3)
